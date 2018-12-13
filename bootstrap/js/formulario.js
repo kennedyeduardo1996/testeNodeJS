af468b9b6985eada553d60c828cb482f17289b33
kennedyeduardo1996 <kennedyeduardomartins@gmail.com>
2018-12-12 23:54:12 -0200

    formulario ok

--- bootstrap/js/formulario.js
+++ bootstrap/js/formulario.js
@@ -3,14 +3,16 @@ $(document).ready(function () {
     $("input[type=radio]").on('click',  function(){
         var id = $(this).prop('id');
         if (id == 'radio_1'){
-
-
-
-       }else if (id == 'radio_2'){
-            $(".hiden").show();
-
-
-       }
+            $(".hiden").slideUp(2000);
+            // $(".hiden").hide(2000);
+            // $(".hiden").fadeOut(2000);
+            // $(".hiden").slideToggle(2000);
+        }else if (id == 'radio_2'){
+            $(".hiden").slideDown(2000);
+            // $(".hiden").show(2000);
+            // $(".hiden").fadeIn(2000);
+            // $(".hiden").slideToggle(2000);
+        }
     });
 
 
